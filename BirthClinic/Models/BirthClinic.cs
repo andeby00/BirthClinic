6cb256f467a954242db18c2da043323e2db9d1c1
Kia Thomsen <kbt@enversion.dk>
2021-04-07 15:18:17 +0200

    Små ændringer i properties

--- BirthClinic/Models/BirthClinic.cs
+++ BirthClinic/Models/BirthClinic.cs
@@ -8,7 +8,7 @@ namespace BirthClinic.Models
     {
         public int ID { get; set; }
         public string ClinicName { get; set; }
-        public string? Address { get; set; }
+        public string Address { get; set; }
         public List<Clinician> Clinicians { get; set; }
         public List<BirthRoom> BirthRooms { get; set; }
         public List<MaternityRoom> MaternityRooms { get; set; }
