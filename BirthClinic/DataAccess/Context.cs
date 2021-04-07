2b06f94a017ab0a0149279b86e82a5c715aea2c6
Anders Schou <aspschou@live.dk>
2021-04-07 15:24:59 +0200

    added dbset

--- BirthClinic/DataAccess/Context.cs
+++ BirthClinic/DataAccess/Context.cs
@@ -8,6 +8,21 @@ namespace BirthClinic.DataAccess
 {
     public class Context : DbContext
     {
+
+        public DbSet<Birth> births { get; set; }
+        public DbSet<BirthClinic> birthClinics { get; set; }
+        public DbSet<BirthRoom> birthRooms { get; set; }
+        public DbSet<Child> children { get; set; }
+        public DbSet<Clinician> clinicians { get; set; }
+        public DbSet<Doctor> doctors { get; set; }
+        public DbSet<MaternityRoom> maternityRooms { get; set; }
+        public DbSet<Midwife> midwives { get; set; }
+        public DbSet<Nurse> nurses { get; set; }
+        public DbSet<Parent> parents { get; set; }
+        public DbSet<RestRoom> restRooms { get; set; }
+        public DbSet<Secretary> secretaries { get; set; }
+        public DbSet<SocialHealthAss> socialHealthAsses { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder ob)
         {
             ob.UseSqlServer(
